4c9610224ebdd40e435ab3131a06f6c3d67729c6
Zusendo <thaboldt@gmail.com>
2019-05-06 21:31:00 +0200

    editor manifacturing

--- project/Avrina/Assets/Scripts/MagicSystem/MagicSystemSettings.cs
+++ project/Avrina/Assets/Scripts/MagicSystem/MagicSystemSettings.cs
@@ -9,13 +9,11 @@ public class MagicSystemSettings : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 
     public bool isMagicSystemKeyPressed(MagicSystemKey key)
--- project/Avrina/Assets/Scripts/_Editor/MagicSystem/MagicSystemSettingsEditor.cs
+++ project/Avrina/Assets/Scripts/_Editor/MagicSystem/MagicSystemSettingsEditor.cs
@@ -6,6 +6,8 @@ using System.Collections.Generic;
 [CustomEditor(typeof(MagicSystemSettings))]
 public class MagicSystemSettingsEditor : Editor
 {
+    private bool showKeyArray = false;
+
     private void OnEnable()
     {
         // Generate KeyMapper
@@ -16,6 +18,8 @@ public class MagicSystemSettingsEditor : Editor
         // Only regenerate if the mapping does not exist or the enum changed
         if (settings.keyMapper == null)
         {
+            settings.keyMapper = new Dictionary<MagicSystemKey, KeyCode>();
+
             foreach (var magicKey in magicKeys)
             {
                 // Default Key is space because it does not make any sence
@@ -40,8 +44,38 @@ public class MagicSystemSettingsEditor : Editor
         }
     }
 
-    private void OnIspectorGUI()
+    public override void OnInspectorGUI()
     {
-        
+        // Generate KeyMapper
+        var settings = (MagicSystemSettings)target;
+
+        // Create Section where the magic keys will be shown only if the area is open
+        this.showKeyArray = EditorGUILayout.Foldout(this.showKeyArray, "Magic Keys:");
+
+        if (this.showKeyArray)
+        {
+            var magicKeys = (MagicSystemKey[])Enum.GetValues(typeof(MagicSystemKey));
+
+            // Go through all Magic keys and draw them
+            foreach (var magicKey in magicKeys)
+            {
+                EditorGUILayout.BeginHorizontal();
+                
+                EditorGUILayout.LabelField(magicKey.ToString());
+                if (GUILayout.Button(settings.keyMapper[magicKey].ToString()))
+                {
+                    foreach (KeyCode key in Enum.GetValues(typeof(KeyCode)))
+                    {
+                        if (Input.GetKey(key))
+                        {
+                            Debug.Log(key.ToString());
+                            settings.keyMapper[magicKey] = key;
+                        }
+                    }
+                }
+
+                EditorGUILayout.EndHorizontal();
+            }
+        }
     }
 }
